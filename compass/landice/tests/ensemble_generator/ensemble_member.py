573b5640f4f7bae9f98dc080ab4d68192b83a3a3
Matthew Hoffman <mhoffman@lanl.gov>
2024-10-09 12:41:31 -0700

    add runoff file

--- compass/landice/tests/ensemble_generator/ensemble_member.py
+++ compass/landice/tests/ensemble_generator/ensemble_member.py
@@ -263,6 +263,8 @@ class EnsembleMember(Step):
         stream_replacements['TF_file_path'] = TF_file_path
         SMB_file_path = section.get('SMB_file_path')
         stream_replacements['SMB_file_path'] = SMB_file_path
+        runoff_file_path = section.get('runoff_file_path')
+        stream_replacements['funoff_file_path'] = runoff_file_path
 
         # store accumulated namelist and streams options
         self.add_namelist_options(options=options,
